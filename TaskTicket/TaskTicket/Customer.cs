c8c2eb67307fb63f518fdbbc2369897ce8a72e0d
Ekoodi-Jouni <jouni.k.kononen@gmail.com>
2018-01-19 16:49:44 +0200

    TaskTicket updated price datatype changed

---
+++ TaskTicket/TaskTicket/Customer.cs
@@ -0,0 +1,105 @@
+﻿namespace Exam5
+{
+    /// <summary>
+    /// Customer data and logic for calculating price discounts
+    /// </summary>
+    class Customer
+    {
+        // base price (without discounts) for customer
+        private const double BASEPRICE = 16;
+
+        private string _name = null;
+        private int _age = 0;
+        private bool _isStudent = false;
+        private bool _isConscript = false;
+        private bool _isMTKmember = false;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name"> customer's name </param>
+        public Customer(string name)
+        {
+            _name = name;
+        }
+
+        /// <summary>
+        /// Properties
+        /// set and get
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public bool IsStudent
+        {
+            get { return _isStudent; }
+            set { _isStudent = value; }
+        }
+
+        public bool IsConscript
+        {
+            get { return _isConscript; }
+            set { _isConscript = value; }
+        }
+
+        public bool IsMTKmember
+        {
+            get { return _isMTKmember; }
+            set { _isMTKmember = value; }
+        }
+
+
+        /// <summary>
+        /// Calculate actual price for customer
+        /// </summary>
+        /// <returns> actual price with discounts </returns>
+        public double GetPrice()
+        {
+            double price = BASEPRICE;
+
+            if (Age < 7)
+            {
+                price = 0.00f; // free ticket for children under 7 years old
+            }
+            else if (IsMTKmember && IsStudent)
+            {
+                price *= 0.40f; // 15% + 45% discount if customer is both student and member of MTK
+            }
+            else if (Age >= 65)
+            {
+                price *= 0.50f; // 50% discount for pensioners
+            }
+            else if (Age <= 15)
+            {
+                price *= 0.50f; // 50% discount for young people (age between 7 and 15 (inclusive))
+            }
+            else if (IsConscript)
+            {
+                price *= 0.50f; // 50% discount for conscripts
+            }
+            else if (IsStudent)
+            {
+                price *= 0.55f; // 45% discount for students
+            }
+            else if (IsMTKmember)
+            {
+                price *= 0.85f; // 15% discount for members of MTK
+            }
+            else
+            {
+                // no discount
+            }
+
+            return price;
+        }
+
+    }
+}
---
+++ TaskTicket/TaskTicket/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Exam5
+{
+    /// <summary>
+    /// Exam 5: Read customer data and evaluate ticket price
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter customer data...");
+
+            Console.Write("Enter name : ");
+            var name = Console.ReadLine();
+            var customer = new Customer(name);
+            
+            
+            int age = -1;
+            while (age < 0)
+            {
+                Console.Write("Enter age : ");
+                if (!int.TryParse(Console.ReadLine(), out age)) 
+                {
+                    Console.WriteLine("Invalid age!");
+                    age = -1;
+                }
+                else {
+                    customer.Age = age;
+                }
+            }
+
+            customer.IsMTKmember = YesNo("Is customer a member of MTK");
+            customer.IsConscript = YesNo("Is customer a conscript");
+            customer.IsStudent = YesNo("Is customer a student");
+
+            Console.WriteLine($"Price for {customer.Name} is {customer.GetPrice():F2}");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Accepts only Y or N as answer (ok, lowercase also accepted ;)
+        /// </summary>
+        /// <param name="prompt"> the question </param>
+        /// <returns> true, if Y | false, if N </returns>
+        private static bool YesNo(string prompt)
+        {
+            while (true)
+            {
+                Console.Write("{0} (Y/N) : ", prompt);
+                char c = char.ToUpper(Console.ReadKey().KeyChar);
+                Console.WriteLine();
+                if (c == 'Y')
+                {
+                    return true;
+                }
+                if (c == 'N')
+                {
+                    return false;
+                }
+                Console.WriteLine("Please answer Y or N!");
+            }
+        }
+    }
+}

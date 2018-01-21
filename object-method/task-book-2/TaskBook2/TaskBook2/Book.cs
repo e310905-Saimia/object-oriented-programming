3a568f26875ebd2a31ffa9db3b5fb63b20349977
Ekoodi-Jouni <jouni.k.kononen@gmail.com>
2018-01-21 20:12:32 +0200

    TaskBook 2 initialized

---
+++ object-method/task-book-2/TaskBook2/TaskBook2/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace TaskBook2
+{
+    class Book
+    {
+        private string _name;
+        private string _author;
+        private string _publisher;
+        private double _price;
+        private static string _category;
+
+        public Book()
+        {
+            _name = "unknown";
+            _author = "unknown";
+            _publisher = "unknown";
+            _price = 0;
+            _category = "unknown";
+            
+
+        }
+
+    }
+}

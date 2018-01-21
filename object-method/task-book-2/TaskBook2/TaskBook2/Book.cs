8643ac2197faf1335016ea2ef9107f40d2359cc2
Ekoodi-Jouni <jouni.k.kononen@gmail.com>
2018-01-21 23:14:01 +0200

    TaskBook2 versio 1 - class done - tester done

--- object-method/task-book-2/TaskBook2/TaskBook2/Book.cs
+++ object-method/task-book-2/TaskBook2/TaskBook2/Book.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace TaskBook2
@@ -13,16 +15,81 @@ namespace TaskBook2
         private double _price;
         private static string _category;
 
+        /// <summary>
+        /// Contructor
+        /// </summary>
         public Book()
         {
-            _name = "unknown";
-            _author = "unknown";
-            _publisher = "unknown";
-            _price = 0;
+            Name = "unknown";
+            Author = "unknown";
+            Publisher = "unknown";
+            Price = 0;
             _category = "unknown";
-            
-
         }
 
+        public Book(string name, string author, string publisher, double price, string category)
+        {
+            Name = name;
+            Author = author;
+            Publisher = publisher;
+            Price = price;
+            _category = category;
+        }
+
+
+        /// <summary>
+        /// Encapsulated properties
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Author
+        {
+            get { return _author; }
+            set { _author = value; }
+        }
+
+        public string Publisher
+        {
+            get { return _publisher; }
+            set { _publisher = value; }
+        }
+
+        public double Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        public static string Category
+        {
+            get { return _category; }
+            //set { _category = value; }
+        }
+
+        public void GetBook(string name)
+        {
+            if (Name == name)
+            {
+                Console.WriteLine($"Kirja: {Name}\n" +
+                                  $"Kijailija: {Author}\n" +
+                                  $"Julkaisija: {Publisher}\n" +
+                                  $"Hinta: {Price}\n" +
+                                  $"----------------------");
+            }
+            else
+            {
+                Console.WriteLine($"{name} kirjaa ei löydy!");
+            }
+        }
+
+        public static void ChangeCategory(string category)
+        {
+            _category = category;
+        }
     }
 }
+
--- object-method/task-book-2/TaskBook2/TaskBook2/Program.cs
+++ object-method/task-book-2/TaskBook2/TaskBook2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace TaskBook2
 {
@@ -6,7 +7,30 @@ namespace TaskBook2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Book[] books = new Book[5];
+            GreateData(books);
+            Console.WriteLine("Book luokan testeri!");
+            Console.WriteLine($"Kategoria nyt: {Book.Category}");
+            Book.ChangeCategory("Sota");
+            Console.WriteLine($"Kategoria nyt: {Book.Category}");
+
+
+            for (int i = 0; i < books.Length; i++)
+            {
+                books[i].GetBook("Tuntematon sotilas");
+            }
+            
+        }
+
+        public static Book[] GreateData(Book[] books)
+        {            
+            books[0]=new Book("Häräntappo ase", "Anna-Leena Härkönen", "Otava", 10,"Draama");
+            books[1] = new Book("Tuntematon sotilas", "Väinö Linna", "Otava", 100, "Sota");
+            books[2] = new Book("Sinuhe egyptiläinen", "Mika Waltari", "Otava", 25, "Draama");
+            books[3] = new Book("Uhrilento", "Ilkka Remes", "VSOY", 25, "Jännitys");
+            books[4] = new Book("Mielensäpahoittaja", "", "Otava", 50, "Huumori");
+
+            return books;
         }
     }
 }

caa4b2772eb0d0645cb0b9f0a638066e669bb798
Ekoodi-Jouni <jouni.k.kononen@gmail.com>
2018-01-22 23:34:58 +0200

    Inherit tester done

--- object-method/TaskVehile/TaskVehile/Program.cs
+++ object-method/TaskVehile/TaskVehile/Program.cs
@@ -11,9 +11,11 @@ namespace TaskVehile
             Vehicle vehicle = new Vehicle("Henkilöauto", "Audi", 2010, 25000);
             Car car = new Car("Henkilöauto", "VW", 2012,20000,2.0,"Passat",5);
 
-           vehicle.PrintVehileInfo();
+            vehicle.PrintVehileInfo();
             car.PrintVehileInfo();
-            
+
+            Car car2 = new Car("Pakettiauto", "Ford", 2015, 35000,3.3,"Transit",4);
+            car2.PrintVehileInfo();
         }
     }
 }

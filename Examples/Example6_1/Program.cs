d8b56cc4e7027a5072738557f538d2b20dca755e
Ekoodi-Jouni <jouni.k.kononen@gmail.com>
2018-01-23 23:46:03 +0200

    Examples initialized

---
+++ Examples/Example6_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tässä luodaan auto1-olio oletusmuodostimella.
+            
+            Auto auto1 = new Auto();
+           
+            //Tässä kutsutaan staattinen metodi AutojenLukumaara() 
+            //suoraan luokan nimen kautta.
+            Auto.AutojenLukumaara();
+
+            //Tässä luodaan auto2-olio kaikilla parametreilla.
+            Auto auto2 = new Auto("Ford", "Focus", 1999, 1.6m, 5800);
+
+            //Tässä kutsutaan staattinen metodi AutojenLukumaara() 
+            //suoraan luokan nimen kautta.
+            Auto.AutojenLukumaara();
+
+            //Tässä luodaan auto3-olio kopiointimuodostimella.
+            Auto auto3 = new Auto(auto2);
+
+            //Tässä kutsutaan staattinen metodi AutojenLukumaara() 
+            //suoraan luokan nimen kautta.
+            Auto.AutojenLukumaara();
+        }
+    }
+}
---
+++ Examples/Example6_5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JohtajaFirma;
+
+
+namespace Example6_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tässä määritellään jokuJohtaja-olio.
+            Johtaja jokuJohtaja = new Johtaja("Johnny Logan",
+                100000.0m);
+
+            //Tässä määritellään ekaFirma-olio.
+            Firma ekaFirma = new Firma("Soft Intelligenece",
+                10000000, jokuJohtaja);
+
+            //Tässä kutsutaan ekaFirma-olion FirmanTiedot()-metodi.
+            ekaFirma.FirmanTiedot();
+
+
+            //Tässä määritellään tokaFirma-olio. Huomaa, kuinka 
+            //lennossa luodaan uusi Johtaja-olio oletusmuodostimella 
+            //seuraavalla lauseella: new Johtaja().
+
+            Firma tokaFirma = new Firma("Easy Money", 500000, new
+                Johtaja());
+
+            //Tässä kutsutaan tokaFirma-olion FirmanTiedot()-metodi.
+            tokaFirma.FirmanTiedot();
+        }
+    }
+}

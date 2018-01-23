131a35d4a9575d6be282c21c5d36697ffee3b7c1
Ekoodi-Jouni <jouni.k.kononen@gmail.com>
2018-01-23 23:44:04 +0200

    ClassDiagram Example added

--- object-method/TaskVehile/TaskVehicle_2/Program.cs
+++ object-method/TaskVehile/TaskVehicle_2/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using TaskVehicle;
 
-namespace TaskVehicle
+
+namespace TaskVehicle_2
 {
     class Program
     {
@@ -9,12 +9,12 @@ namespace TaskVehicle
         {
             Console.WriteLine("Perintä testeri");
             Vehicle vehicle = new Vehicle("Henkilöauto", "Audi", 2010, 25000);
-            Car car = new Car("Henkilöauto", "VW", 2012,20000,2.0,"Passat",5);
+            Car car = new Car("Henkilöauto", "VW", 2012, 20000, 2.0, "Passat", 5);
 
             vehicle.PrintVehileInfo();
             car.PrintVehileInfo();
 
-            Car car2 = new Car("Pakettiauto", "Ford", 2015, 35000,3.3,"Transit",4);
+            Car car2 = new Car("Pakettiauto", "Ford", 2015, 35000, 3.3, "Transit", 4);
             car2.PrintVehileInfo();
         }
     }
---
+++ object-method/TaskVehile/TaskVehicle_2/Vehicle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskVehicle_2
+{
+    class Vehicle
+    {
+        protected string _type;
+        protected string _brand;
+        protected int _year;
+        protected double _price;
+
+        public Vehicle(string type, string brand, int yearOfModel, double price)
+        {
+            _type = type;
+            _brand = brand;
+            _year = yearOfModel;
+            _price = price;
+        }
+
+        public virtual void PrintVehileInfo()
+        {
+            Console.WriteLine($"==================\n" +
+                              $"Tyyppi: {_type}\n" +
+                              $"Merkki: {_brand}\n" +
+                              $"Vuosimalli: {_year}\n" +
+                              $"Hinta: {_price}");
+
+        }
+    }
+}

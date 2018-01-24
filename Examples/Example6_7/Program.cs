2b608169d6449acaff4345e2eba8a24865d8a8b9
Ekoodi-Jouni <jouni.k.kononen@gmail.com>
2018-01-24 23:10:57 +0200

    Example6_7 done

--- Examples/Example6_7/Program.cs
+++ Examples/Example6_7/Program.cs
@@ -4,12 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Example6_7
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Tässä luodaan asiakas-olio, joka on instanssi  
+            //Sovellus.Kayttoliittyma.Asiakas-luokasta. Huomaa, 
+            //että luokan nimi joudutaan merkitsemään 
+            //nimiavaruuksien kautta. 
+
+            Sovellus.Kayttoliittyma.Asiakas asiakas = new
+                Sovellus.Kayttoliittyma.Asiakas("Sara");
+
+            Console.WriteLine("\nSovellus.Kayttoliittyma.Asiakas.TarkistaAsiakas()-metodin tuloste:");
+
+            //Tässä kutsutaan TarkistaAsiakas()-metodi.
+            asiakas.TarkistaAsiakas("Susan");
+
+            Console.WriteLine("\nSovellus.Kayttoliittyma.Asiakas.TarkistaAsiakas()-metodin tuloste:");
+
+            //Tässä kutsutaan taas TarkistaAsiakas()-metodi 
+            //uudella parametrin arvolla.
+            asiakas.TarkistaAsiakas("Sara");
+
+            Console.WriteLine("\nSovellus.Tietokantayhteys.AvaaYhteys-muodostimen tuloste:");
+
+            //Tässä luodaan yhteys-olio, joka on instanssi 
+            //Sovellus.Tietokantayhteys.AvaaYhteys-luokasta.
+            Sovellus.Tietokantayhteys.AvaaYhteys yhteys = new
+                Sovellus.Tietokantayhteys.AvaaYhteys();
         }
     }
 }

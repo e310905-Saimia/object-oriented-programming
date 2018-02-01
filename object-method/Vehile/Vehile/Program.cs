6444ef10aecd0db1c4826bb907c1e8441183cc24
Ekoodi-Jouni <jouni.k.kononen@gmail.com>
2018-02-01 23:51:20 +0200

    Inheritance task - Vehile Initialized Classes defined

---
+++ object-method/Vehile/Vehile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using VehileXXX;
+
+namespace Vehile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle v1 = new Vehicle("Mopo", "Solifer", 1960, 1500);
+            Car c1 = new Car("Auto", "Audi", 2015, 26000,3.3,"Quatro",4);
+
+            Console.WriteLine(v1.PrintData());
+            Console.WriteLine(c1.PrintData());
+
+            Console.WriteLine(v1.ToString());
+            Console.WriteLine(c1.ToString());
+
+        }
+    }
+}
---
+++ object-method/Vehile/Vehile/Vehicle.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehileXXX
+{
+    class Vehicle
+    {
+        protected string Type;
+        protected string Brand;
+        protected int Year;
+        protected double Price;
+
+
+        public Vehicle(string type, string brand, int year, double price)
+        {
+            Type = type;
+            Brand = brand;
+            Year = year;
+            Price = price;
+        }
+
+        public virtual bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public virtual string PrintData()
+        {
+            return $"-------------\n" +
+                   $"Tyyppi: {Type}\n" +
+                   $"Merkki: {Brand}\n" +
+                   $"Vuosimalli: {Year}\n" +
+                   $"Hinta: {Price}\n" +
+                   $"==============\n";
+        }
+
+        public override string ToString()
+        {
+            return PrintData();
+        }
+    }
+
+    class Car : Vehicle
+    {
+        //Fields
+        protected double Engine;
+        protected string Model;
+        protected int Doors;
+
+       public Car(string type, string brand, int year, double price, double engine, string model, int doors) : base(type, brand, year, price)
+        {
+            Engine = engine;
+            Model = model;
+            Doors = doors;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override string PrintData()
+        {
+            return $"{base.PrintData()}\n" +
+                   $"Moottori: {Engine}\n" +
+                   $"Malli: {Model}\n" +
+                   $"Ovien määrä: {Doors}\n" +
+                   $"xxxxxxxxxxxxxxxxx\n";
+        }
+
+        public override string ToString()
+        {
+            return PrintData();
+        }
+    }
+}

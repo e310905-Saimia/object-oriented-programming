86a69ff7ba0058bfecd517211f8787230573c7f5
Ekoodi-Jouni <jouni.k.kononen@gmail.com>
2018-02-11 21:47:37 +0200

    BBan Utility defined

---
+++ bank-util-3/Bban/Bban/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Bban
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string machineFormat = BankUtil.CorrectNumber("227720-35988");
+            Console.WriteLine(BankUtil.IsValidAccount(machineFormat));
+        }
+    }
+}

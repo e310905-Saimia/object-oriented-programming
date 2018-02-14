fe7926c1f84b31150fa6db4fc84505c585fe0256
Ekoodi-Jouni <jouni.k.kononen@gmail.com>
2018-02-14 09:21:19 +0200

    Interface Example8_1 Initialized

---
+++ Examples/Example8_1/Esimerkki8-1.cs
@@ -0,0 +1,75 @@
+using System;
+
+//Seuraavassa m‰‰ritell‰‰n liittym‰ IRobotti.
+public interface IRobotti
+{
+    //T‰ss‰ esitell‰‰n liittym‰n metodit
+    void Aloita();
+    void Lopeta();
+
+    //T‰ss‰ esitell‰‰n ominaisuus RobottiToiminnassa.
+    bool RobottiToiminnassa
+    {
+        get;
+    }
+}
+
+//Seuraavassa m‰‰ritell‰‰n luokka Robotti.
+public class Robotti : IRobotti
+{
+    //T‰ss‰ m‰‰ritell‰‰n robottiToiminnassa-kentt‰, jota
+    //liittym‰n RobottiToiminnassa-property k‰ytt‰‰. 
+    private bool robottiToiminnassa = false;
+
+    //Seuraavassa toteutetaan liittym‰n Aloita()-metodi.
+    //Huomaa, ett‰ metodin saantim‰‰reen pit‰‰ olla public. 
+    public void Aloita()
+    {
+        robottiToiminnassa = true;
+        Console.WriteLine("Robotti aloittaa toiminnan...");
+    }
+
+    //Seuraavassa toteutetaan liittym‰n Lopeta()-metodi. 
+    //Huomaa, ett‰ metodin saantim‰‰reen pit‰‰ olla public! 
+    public void Lopeta()
+    {
+        Console.WriteLine("Robotti lopettaa toiminnan...");
+        robottiToiminnassa = false;
+    }
+
+    //Seuraavassa toteutetaan RobottiToiminnassa-property.  
+    //Huomaa, ett‰ property:n saantim‰‰reen pit‰‰ olla public!
+    public bool RobottiToiminnassa
+    {
+        get
+        {
+            return robottiToiminnassa;
+        }
+    }
+}
+
+class Esimerkki8_1
+{
+    static void Main(string[] args)
+    {
+        //T‰ss‰ luodaan robotti-olio.
+        Robotti robotti = new Robotti();
+
+        //T‰ss‰ kutsutaan RobottiToiminnassa-property.
+        Console.WriteLine("Robotti toiminnassa: " +
+        robotti.RobottiToiminnassa);
+
+        //Seuraavassa kutsutaan olion metodit.
+        robotti.Aloita();
+
+        //T‰ss‰ kutsutaan RobottiToiminnassa-property.
+        Console.WriteLine("Robotti toiminnassa: " +
+        robotti.RobottiToiminnassa);
+
+        robotti.Lopeta();
+
+        //T‰ss‰ kutsutaan RobottiToiminnassa-property.
+        Console.WriteLine("Robotti toiminnassa: " +
+        robotti.RobottiToiminnassa);
+    }
+}

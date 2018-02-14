72fa40ceecac8579af8736f4d7778279c0786744
Ekoodi-Jouni <jouni.k.kononen@gmail.com>
2018-02-14 09:09:02 +0200

    Bban Class defined

--- bank-util-3/Bban/Bban/Program.cs
+++ bank-util-3/Bban/Bban/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DigiTrade.Utility;
 
 namespace Bban
 {
@@ -6,8 +7,24 @@ namespace Bban
     {
         static void Main(string[] args)
         {
-            string machineFormat = BankUtil.CorrectNumber("227720-35988");
-            Console.WriteLine(BankUtil.IsValidAccount(machineFormat));
+            Bban newAcount = new Bban("227720A35988");
+            //string machineFormat = newAcount.Bban1;
+            //newAcount.IsMachineNumber = BankUtil.CorrectNumber(ref machineFormat);
+            //newAcount.Bban1 = machineFormat;
+            Console.WriteLine(newAcount.ToString());
+
+            Bban account_2 = new Bban("123456-781");
+            Console.WriteLine(account_2.ToString());
+
+
+            //Console.WriteLine($"{newAcount.Bban1} - {newAcount.IsValid()}");
+
+            //string machineFormat = "A227720-35988";
+            //bool isMachineFormat = BankUtil.CorrectNumber(ref machineFormat);
+            //if (isMachineFormat)
+            //    Console.WriteLine($"{machineFormat} - {BankUtil.IsValidAccount(machineFormat)}");
+            //else
+            //    Console.WriteLine("Account number incorrect!");
         }
     }
 }
---
+++ bank-util-3/Bban/Bban/bban.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DigiTrade.Utility;
+
+namespace Bban
+{
+    class Bban
+    {
+        public string Account { get; set; }
+        public bool IsMachineNumber { get; set; }
+        public string MachineNumber { get; set; }
+
+
+        public Bban(string bban)
+        {            
+            Account = bban;
+            string tmpRefAccount = Account;
+            IsMachineNumber = BankUtil.CorrectNumber(ref tmpRefAccount);
+            MachineNumber = tmpRefAccount;
+        }
+
+        public bool IsValid()
+        {
+            return BankUtil.IsValidAccount(Account);
+        }
+
+        public override string ToString()
+        {
+            return $"BBAN: {Account}\n" +                   
+                   $"MachineNumber: {MachineNumber}\n" +
+                   $"IsValid: {IsMachineNumber}\n" +
+                   $"--------------------------\n";
+
+        }
+    }
+}
